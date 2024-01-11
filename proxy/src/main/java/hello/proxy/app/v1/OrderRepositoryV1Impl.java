826217d5e404e16d6609ac073c2cb426f1ca00d2
hangyeol0531 <jhan0531@naver.com>
2024-01-12 00:29:31 +0900

    proxy bean DI init

---
+++ proxy/src/main/java/hello/proxy/app/v1/OrderRepositoryV1Impl.java
@@ -0,0 +1,19 @@
+package hello.proxy.app.v1;
+
+public class OrderRepositoryV1Impl implements OrderRepositoryV1 {
+    @Override
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("exception!!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
