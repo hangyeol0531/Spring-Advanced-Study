2f6b432dada7375fb283d6888f4d5de71d72f2e7
hangyeol0531 <jhan0531@naver.com>
2024-01-22 23:25:09 +0900

    spring aop impl

---
+++ aop/src/main/java/hello/aop/order/aop/Pointcuts.java
@@ -0,0 +1,23 @@
+package hello.aop.order.aop;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+// 외부에서 포이트컷 참조시 접근 제어자 public으로 해야함
+public class Pointcuts {
+
+    //hello.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    public void allOrder() {
+    } // pointcut signature
+
+    //class name pattern *Service
+    @Pointcut("execution(* *..*Service.*(..))")
+    public void allService() {
+
+    }
+
+    @Pointcut("allOrder() && allService()")
+    public void orderAndService() {
+
+    }
+}
--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,16 +2,24 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4Pointcut.class)
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired OrderService orderService;
@@ -32,6 +40,6 @@ public class AopTest {
     @Test
     void exception() {
         assertThatThrownBy(() -> orderService.orderItem("ex"))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalStateException.class);
     }
 }
